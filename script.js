ef79e026bcc986b4ae447979b7fd323c09e44f72
Joseph Cox <joeyhobart@outlook.com>
2022-02-19 23:52:05 -0500

    Initial setup with albums

---
+++ script.js
@@ -0,0 +1,57 @@
+const page_home = document.getElementsByClassName("page_home")[0]
+const page_about = document.getElementsByClassName("page_about")[0]
+
+const pages = document.getElementsByClassName("page")
+
+const nav_home = document.getElementById("header_home")
+
+const albums = document.getElementsByClassName("albumimg")
+const album_lc = document.getElementsByClassName("album_link_container")
+
+const darkmodetoggle = document.getElementById("darkmodetoggle")
+const body = document.body
+
+nav_home.addEventListener('click', function() {
+    navigate("home");
+}, false)
+
+function navigate(location) {
+    if (location != "home") {
+        for (let i=0;i<pages.length;i++) {
+            if (pages[i].classList.contains("page_"+location)) {
+                pages[i].classList.remove('hidden')
+            } else {
+                pages[i].classList.add('hidden')
+            }
+        }
+    } else {
+        for (let i=0;i<pages.length;i++) {
+            pages[i].classList.remove('hidden')
+        }
+    }
+
+}
+
+darkmodetoggle.addEventListener('click', function() {
+    console.log("test", darkmodetoggle.dataset.dark)
+    if (darkmodetoggle.dataset.dark == "true") {
+        body.style.color = "black"
+        body.style.backgroundColor = "white"
+        darkmodetoggle.dataset.dark = "false"
+        darkmodetoggle.innerHTML = "☀️"
+    } else {
+        body.style.color = "white"
+        body.style.backgroundColor = "black"
+        darkmodetoggle.dataset.dark = "true"
+        darkmodetoggle.innerHTML = "🌙"
+
+    }
+}, false)
+
+for (let i=0;i<albums.length;i++) {
+    albums[i].addEventListener('click', function() {
+        album_lc[i].classList.toggle('collapsed')
+        album_lc[i].classList.toggle('expanded')
+        console.log("test")
+    }, false)
+}
